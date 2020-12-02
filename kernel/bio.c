f31380dc1cba4c857dc4af2721ff597fee78efbc
h1053250212 <1053250212@qq.com>
2020-12-02 09:14:32 +0800

    Lab3

--- kernel/bio.c
+++ kernel/bio.c
@@ -23,34 +23,46 @@
 #include "fs.h"
 #include "buf.h"
 
+#define NBUCKETS 13
+ 
 struct {
-  struct spinlock lock;
+  // 每个bucket一个lock
+  // hashbucket.next是当前bucket的MRU
+  struct spinlock lock[NBUCKETS];
   struct buf buf[NBUF];
-
-  // Linked list of all buffers, through prev/next.
-  // head.next is most recently used.
-  struct buf head;
+  struct buf hashbucket[NBUCKETS];
 } bcache;
-
+ 
+ 
+int bhash(int blockno){
+  return blockno%NBUCKETS;
+}
+ 
+ 
 void
 binit(void)
 {
   struct buf *b;
-
-  initlock(&bcache.lock, "bcache");
-
-  // Create linked list of buffers
-  bcache.head.prev = &bcache.head;
-  bcache.head.next = &bcache.head;
+  for(int i=0;i<NBUCKETS;i++){
+    initlock(&bcache.lock[i], "bcache.bucket");
+    // 仍然将每个bucket的头节点都指向自己
+    b=&bcache.hashbucket[i];
+    b->prev = b;
+    b->next = b;
+  }
+ 
+  // 此时因为buffer没有和磁盘块对应起来，所以blockno全部为初始值0，将其全部放在第一个bucket中
+  // 至于其他bucket缺少buffer该怎么解决，在bget里阐述
   for(b = bcache.buf; b < bcache.buf+NBUF; b++){
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
+    // 虽然看起来复杂了点，但仍然是插在表头
+    b->next = bcache.hashbucket[0].next;
+    b->prev = &bcache.hashbucket[0];
     initsleeplock(&b->lock, "buffer");
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
+    bcache.hashbucket[0].next->prev = b;
+    bcache.hashbucket[0].next = b;
   }
 }
-
+ 
 // Look through buffer cache for block on device dev.
 // If not found, allocate a buffer.
 // In either case, return locked buffer.
@@ -58,40 +70,60 @@ static struct buf*
 bget(uint dev, uint blockno)
 {
   struct buf *b;
-
-  acquire(&bcache.lock);
-
-  // Is the block already cached?
-  for(b = bcache.head.next; b != &bcache.head; b = b->next){
+  int h=bhash(blockno);
+  acquire(&bcache.lock[h]);
+ 
+  // 首先在blockno对应的bucket中找，refcnt可能为0，也可能不为0
+  for(b = bcache.hashbucket[h].next; b != &bcache.hashbucket[h]; b = b->next){
     if(b->dev == dev && b->blockno == blockno){
       b->refcnt++;
-      release(&bcache.lock);
+      release(&bcache.lock[h]);
       acquiresleep(&b->lock);
       return b;
     }
   }
-
-  // Not cached; recycle an unused buffer.
-  for(b = bcache.head.prev; b != &bcache.head; b = b->prev){
-    if(b->refcnt == 0) {
-      b->dev = dev;
-      b->blockno = blockno;
-      b->valid = 0;
-      b->refcnt = 1;
-      release(&bcache.lock);
-      acquiresleep(&b->lock);
-      return b;
+ 
+  // 如果在h对应的bucket中没有找到，那么需要到其他bucket中找，这种情况不会少见，因为
+  // binit中，我们就把所有的buffer都插入到了第一个bucket中（当时blockno都是0
+  // 此时原来bucket的锁还没有释放，因为我们在其他bucket中找到buffer后，还要将其插入到原bucket中
+  int nh=(h+1)%NBUCKETS; // nh表示下一个要探索的bucket，当它重新变成h，说明所有的buffer都bussy（refcnt不为0），此时
+              // 如之前设计的，panic
+  while(nh!=h){
+    acquire(&bcache.lock[nh]);// 获取当前bocket的锁
+    for(b = bcache.hashbucket[nh].prev; b != &bcache.hashbucket[nh]; b = b->prev){
+      if(b->refcnt == 0) {
+        b->dev = dev;
+        b->blockno = blockno;
+        b->valid = 0;
+        b->refcnt = 1;
+        // 从原来bucket的链表中断开
+        b->next->prev=b->prev;
+        b->prev->next=b->next;
+        release(&bcache.lock[nh]);
+        // 插入到blockno对应的bucket中去
+        // 👇就是有头节点的头插法
+        b->next=bcache.hashbucket[h].next;
+        b->prev=&bcache.hashbucket[h];
+        bcache.hashbucket[h].next->prev=b;
+        bcache.hashbucket[h].next=b;
+        release(&bcache.lock[h]);
+        acquiresleep(&b->lock);
+        return b;
+      }
     }
+    // 如果当前bucket里没有找到，在转到下一个bucket之前，记得释放当前bucket的锁
+    release(&bcache.lock[nh]);
+    nh=(nh+1)%NBUCKETS;
   }
   panic("bget: no buffers");
 }
-
+ 
 // Return a locked buf with the contents of the indicated block.
 struct buf*
 bread(uint dev, uint blockno)
 {
   struct buf *b;
-
+ 
   b = bget(dev, blockno);
   if(!b->valid) {
     virtio_disk_rw(b->dev, b, 0);
@@ -99,7 +131,7 @@ bread(uint dev, uint blockno)
   }
   return b;
 }
-
+ 
 // Write b's contents to disk.  Must be locked.
 void
 bwrite(struct buf *b)
@@ -108,7 +140,7 @@ bwrite(struct buf *b)
     panic("bwrite");
   virtio_disk_rw(b->dev, b, 1);
 }
-
+ 
 // Release a locked buffer.
 // Move to the head of the MRU list.
 void
@@ -116,36 +148,37 @@ brelse(struct buf *b)
 {
   if(!holdingsleep(&b->lock))
     panic("brelse");
-
+ 
   releasesleep(&b->lock);
-
-  acquire(&bcache.lock);
+  int h=bhash(b->blockno);
+  acquire(&bcache.lock[h]);
   b->refcnt--;
   if (b->refcnt == 0) {
     // no one is waiting for it.
+    // 下面做的就是把b从原来的位置取下来 放在链表开头（头插法）
     b->next->prev = b->prev;
     b->prev->next = b->next;
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
+    b->next = bcache.hashbucket[h].next;
+    b->prev = &bcache.hashbucket[h];
+    bcache.hashbucket[h].next->prev = b;
+    bcache.hashbucket[h].next = b;
   }
   
-  release(&bcache.lock);
+  release(&bcache.lock[h]);
 }
-
+ 
 void
 bpin(struct buf *b) {
-  acquire(&bcache.lock);
+  int h=bhash(b->blockno);
+  acquire(&bcache.lock[h]);
   b->refcnt++;
-  release(&bcache.lock);
+  release(&bcache.lock[h]);
 }
-
+ 
 void
 bunpin(struct buf *b) {
-  acquire(&bcache.lock);
+  int h=bhash(b->blockno);
+  acquire(&bcache.lock[h]);
   b->refcnt--;
-  release(&bcache.lock);
+  release(&bcache.lock[h]);
 }
-
-

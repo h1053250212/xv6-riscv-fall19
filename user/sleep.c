9f45f197476b88642656b6afd6d5fdd4c60365f1
h1053250212 <1053250212@qq.com>
2020-12-25 03:22:39 +0800

    Lab1

--- user/sleep.c
+++ user/sleep.c
@@ -1,23 +1,13 @@
 #include "kernel/types.h"
 #include "user/user.h"
 #include "kernel/stat.h"
- 
-void usage(){
-    printf("Usage: sleep <NUM> \n");
-    printf("NUM must be non-negative\n");
-    exit();
-}
- 
- 
+
 int main(int argc,char* argv[]){
     if(argc<2){
-        usage();
+        printf("must 1 argument for sleep\n");
+        exit();
     }
-    char c=argv[1][0];
-    if(c<'0'||c>'9'){
-        usage();
-    }
-    // this atoi implementation stops when encounters non-digit
+
     int num=atoi(argv[1]);
     printf("sleep %d ticks\n",num);
     sleep(num);
